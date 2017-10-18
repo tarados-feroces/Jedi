8b85b2c2e901f8768119a71939f35013c9e14e46
Alex <ad.kuznetsov.a7@Yandex.ru>
2017-10-18 23:32:46 +0300

    Update setup.sh

--- Jedi/setup.sh
+++ Jedi/setup.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install build-essential
 sudo apt-get install cmake
+sudo apt-get install tree
 sudo apt-get install libgtk2.0-dev
 cd /var/OCV
 tar -xvzf opencv-3.3.0.tar.gz
